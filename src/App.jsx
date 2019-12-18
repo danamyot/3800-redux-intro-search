ba2de259a5480109df2c32db055c09fd499a533a
Dan Amyot <amyot.dan@gmail.com>
2019-12-17 21:02:51 -0500

    Format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react' 
-import Search from './Search.jsx' 
-import SearchResults from './SearchResults.jsx' 
-class App extends Component { 
-    render = () => { 
-        return (<div> 
-            <Search /> 
-            <SearchResults /> 
-        </div>) 
-    } 
-} 
-export default App 
+import React, { Component } from "react";
+import Search from "./Search.jsx";
+import SearchResults from "./SearchResults.jsx";
+class App extends Component {
+  render = () => {
+    return (
+      <div>
+        <Search />
+        <SearchResults />
+      </div>
+    );
+  };
+}
+export default App;
--- src/data.js
+++ src/data.js
@@ -1,17 +1,21 @@
-let items = [{ 
-    id: "ab21233s", 
-    price: 100, 
-    name: "hat", 
-    description: "nice hat" 
-}, { 
-    id: "a51b23s", 
-    price: 200, 
-    name: "gucci hat", 
-    description: "nicest hat" 
-}, { 
-    id: "a623b23s", 
-    price: 20000, 
-    name: "boat", 
-    description: "nice boat" 
-}] 
-export default items 
+let items = [
+  {
+    id: "ab21233s",
+    price: 100,
+    name: "hat",
+    description: "nice hat"
+  },
+  {
+    id: "a51b23s",
+    price: 200,
+    name: "gucci hat",
+    description: "nicest hat"
+  },
+  {
+    id: "a623b23s",
+    price: 20000,
+    name: "boat",
+    description: "nice boat"
+  }
+];
+export default items;
