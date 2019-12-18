b57a35e90ce20fda05fccdddb0e038b92fe9996e
Dan Amyot <amyot.dan@gmail.com>
2019-12-17 21:05:19 -0500

    Question 2

--- src/data.js
+++ src/data.js
@@ -1,21 +1,33 @@
 let items = [
   {
     id: "ab21233s",
-    price: 100,
+    price: 70,
     name: "hat",
     description: "nice hat"
   },
   {
     id: "a51b23s",
-    price: 200,
+    price: 400,
     name: "gucci hat",
     description: "nicest hat"
   },
   {
     id: "a623b23s",
-    price: 20000,
+    price: 70000,
     name: "boat",
     description: "nice boat"
+  },
+  {
+    id: "a203sm",
+    price: 120,
+    name: "sunglasses",
+    description: "nice glasses"
+  },
+  {
+    id: "s8k2hh56s",
+    price: 2000,
+    name: "laptop",
+    description: "nice laptop"
   }
 ];
 export default items;
