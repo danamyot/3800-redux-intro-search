ef88c955f3fde0cbf614544e3d5e794006a6fb44
Dan Amyot <amyot.dan@gmail.com>
2019-12-19 18:36:29 -0500

    refactor of in stock check

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -4,17 +4,16 @@ import data from "./data.js";
 class UnconnectedSearchResults extends Component {
   render = () => {
     const results = data.filter(item => {
-      const matchingItem = item.name.includes(this.props.query);
-      const priceInRange =
-        item.price >= this.props.minPrice && item.price <= this.props.maxPrice;
-      return matchingItem && priceInRange;
+      return (
+        item.name.includes(this.props.query) &&
+        item.price >= this.props.minPrice &&
+        item.price <= this.props.maxPrice &&
+        (this.props.inStockChecked ? item.inStock : true)
+      );
     });
     return (
       <div>
         {results.map(r => {
-          if (this.props.inStockChecked) {
-            return r.inStock ? <div>{r.name}</div> : null;
-          }
           return <div>{r.name}</div>;
         })}
       </div>
